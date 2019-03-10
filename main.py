9c4630bdb34a483b44092f911da8c50a5cca81a0
Rob Jenks <robjenks@gmail.com>
2019-03-10 15:34:27 +0000

    Implement basic training and network evaluation against mnist data

--- main.py
+++ main.py
@@ -6,19 +6,31 @@ from neural_network import NeuralNetwork
 
 def main():
     if not mnist.binary_data_available():
-        print("Generating binary dataset on first-run for faster read times; this is a one-time activity")
+        print("Generating binary dataset on first-run for faster read times; this is a one-time activity...")
         mnist.generate_binary_data()
 
     # Retrieve and normalise mnist data
+    print("Initialising network and datasets...")
     train_data, train_labels = mnist.labelled_training_data()
     test_data, test_labels = mnist.labelled_test_data()
 
     # Use one-hot label representation for CNN classification
-    train_labels = one_hot_labels(train_labels, mnist.label_count())
-    test_labels = one_hot_labels(test_labels, mnist.label_count())
+    train_labels_vec = one_hot_labels(train_labels, mnist.label_count())
+    test_labels_vec = one_hot_labels(test_labels, mnist.label_count())
 
-    nn = NeuralNetwork(10, 5, 20, 0.01, functions.sigmoid)
-    nn.initialise_weight_matrices()
+    network = NeuralNetwork(input_node_count=mnist.image_size(),
+                            hidden_node_count=100,
+                            output_node_count=10,
+                            learning_rate=0.1,
+                            activation_fn=functions.sigmoid)
+
+    print("Training network...")
+    for x in zip(train_data, train_labels_vec):
+        network.train(*x)
+
+    for i in range(20):
+        result = network.execute(test_data[i])
+        print(int(test_labels[i][0]), np.argmax(result), np.max(result))
 
 
 # One-hot representation for labels within the given value range
--- neural_network.py
+++ neural_network.py
@@ -1,5 +1,6 @@
 import numpy as np
-import functions
+from collections import Counter
+import mnist, functions
 
 
 class NeuralNetwork:
@@ -11,8 +12,11 @@ class NeuralNetwork:
         self.hidden_node_count = hidden_node_count
         self.learn_rate = learning_rate
         self.activation_fn = activation_fn
-        self.weight_ih, self.weight_ho = [], []
+        self.weight_ih, self.weight_ho = None, None
 
+        self.initialise_weight_matrices()
+
+    # Build initial weight matrices based around truncated normal distributions
     def initialise_weight_matrices(self):
         # Generate randomised in->hidden weights from a truncated normal dist
         nr = 1. / np.sqrt(self.in_node_count)
@@ -23,5 +27,92 @@ class NeuralNetwork:
         nr = 1. / np.sqrt(self.hidden_node_count)
         trunc_norm = functions.truncated_norm(mean=0, sd=1, low=-nr, high=nr)
         self.weight_ho = trunc_norm.rvs((self.out_node_count, self.hidden_node_count))
+
+    # Evaluate and adjust network weights based upon the provided training data
+    def train(self, input_vec, target_vec):
+        input_vec, target_vec = (np.array(x, ndmin=2).T for x in [input_vec, target_vec])
+
+        # Input -> hidden layer
+        out_vec = np.dot(self.weight_ih, input_vec)
+        output_hidden = self.activation_fn(out_vec)
+
+        # Hidden layer -> output
+        out_vec = np.dot(self.weight_ho, output_hidden)
+        output_network = self.activation_fn(out_vec)
+
+        # Calculate and adjust for output errors
+        output_errors = target_vec - output_network
+        adjustment = np.dot(output_errors * output_network * (1.0 - output_network), output_hidden.T)
+        adjustment *= self.learn_rate
+
+        self.weight_ho += adjustment
+
+        # Calculate and adjust for hidden layer errors
+        hidden_errors = np.dot(self.weight_ho.T, output_errors)
+        adjustment = np.dot(hidden_errors * output_hidden * (1.0 - output_hidden), input_vec.T)
+        adjustment *= self.learn_rate
+
+        self.weight_ih += adjustment
+
+    # Evaluate the given input data against the network
+    def execute(self, input_vec):
+        input_vec = np.array(input_vec, ndmin=2).T
+
+        # Process hidden layer
+        result = self.activation_fn(
+            np.dot(self.weight_ih, input_vec)
+        )
+
+        # Process output layer
+        result = self.activation_fn(
+            np.dot(self.weight_ho, result)
+        )
+
+        return result
+
+    # Evaluate the given data and calculate a confusion matrix
+    def confusion_matrix(self, data, labels):
+        conf = np.zeros((mnist.label_count(), mnist.label_count()), int)
+
+        # Evaluate against the network
+        for i, x in enumerate(data):
+            result = self.execute(x)
+            highest_confidence_prediction = result.argmax()
+
+            # Plot the intersection of { classifier_result, actual_label } in the CM
+            actual = int(labels[i][0])
+            conf[highest_confidence_prediction, actual] += 1
+
+        return conf
+
+    # Returns precision of the network predictions for a given label, based on the provided conf matrix
+    def precision(self, label, confusion_matrix):
+        # (correct predictions / total predictions) for the label
+        column = confusion_matrix[:, label]
+        return confusion_matrix[label, label] / column.sum()
+
+    # Returns recall of the network predictions for a given label, based on the provided conf matrix
+    def recall(self, label, confusion_matrix):
+        row = confusion_matrix[label, :]
+        return confusion_matrix[label, label] / row.sum()
+    
+    # Execute the network and assess performance against the given label set.  Returns (passes, fails)
+    def evaluate(self, data, labels):
+        matches = sum(self.execute(x) == labels[i] for i, x in enumerate(data))
+        return matches, len(data)-matches
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
